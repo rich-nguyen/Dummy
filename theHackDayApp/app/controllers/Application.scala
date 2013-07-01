4401c28703584255af7a65de1587ebb144b1f5d0
rich-nguyen <richnoin@gmail.com>
2013-07-01 23:45:42 +0100

    A Shitty Form

--- theHackDayApp/app/controllers/Application.scala
+++ theHackDayApp/app/controllers/Application.scala
@@ -2,11 +2,13 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
 
 object Application extends Controller {
   
   def index = Action {
-    Ok(views.html.index("Space Mish"))
+    Ok(views.html.index("Space Mish",loginForm))
   }
 
   def dashboard = Action {
@@ -17,6 +19,19 @@ object Application extends Controller {
     request => Ok("Got a request again: " + request)
   }
 
+  // User submits form
+  def login = Action {
+    implicit request =>
+
+      loginForm.bindFromRequest.fold(
+        formError =>
+          BadRequest("error logging in" + formError.errors.mkString(" ")),
+        {
+          case (username, password) => Ok(views.html.dashboard("Dashboard", username :: List("Rich","Ben","Noin")))
+        }
+    )
+  }
+
   // TODO:
   // Show and set persistent data.
   // Who you know and how, ie. invite-only users. OpenID for now.
@@ -24,5 +39,15 @@ object Application extends Controller {
   def newTask = TODO
 
   def deleteTask(id: Long) = TODO
+
+  /**
+   * Describes the login form.
+   */
+  val loginForm = Form(
+    tuple(
+      "name" -> text,
+      "password" -> text
+    )
+  )
   
 }
