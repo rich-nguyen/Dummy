ee6bbf3a0ddbaef044e3ece00c9b39a68b4305d1
richardnguyen <richnoin@gmail.com>
2014-07-29 00:36:01 +0100

    Manual linting

--- nodeApps/src/modules/finite-state-machine.js
+++ nodeApps/src/modules/finite-state-machine.js
@@ -1,24 +1,23 @@
 exports.create = function(configuration)
 {
-	var currentState = configuration.initial || "",
-		transitions = configuration.transitions || [],
-		actions = configuration.actions || {};
+    var currentState = configuration.initial || "",
+        transitions = configuration.transitions || [],
+        actions = configuration.actions || {};
 
-	function findTransition(event) {
-    	for(var i = 0; i < transitions.length; i++) {
-    		var transition = transitions[i]
-        	if (transition.from === currentState &&
-        		transition.event === event)
-        	{
-        		return transition;
-        	}
-    	}
-    	return null;
+    function findTransition(event) {
+        for (var i = 0; i < transitions.length; i++) {
+            var transition = transitions[i];
+            if (transition.from === currentState &&
+               transition.event === event) {
+                return transition;
+            }
+        }
+        return null;
     }
 
     return {
-    	currentState: function(){ return currentState; },
-    	sendEvent: function(event){
+    	currentState: function() { return currentState; },
+    	sendEvent: function(event) {
     		var transition = findTransition(event);
     		if (transition) {
     			currentState = transition.to;
